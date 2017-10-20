5e4f8eff6a5a9662ca6576924abfb3547f773548
MunKeat <lai.munkeat@gmail.com>
2017-10-21 03:31:09 +0800

    Draft implementation for csv manipulation

--- data.py
+++ data.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 
 class Data:
+
     def debug(self, message):
         if conf['debug']:
             print(message)
@@ -26,13 +27,13 @@ class Data:
                          "c_data"],
             # Orders
             'order': ["w_id", "d_id", "o_id", "c_id", "o_carrier_id",
-                       "o_ol_cnt", "o_all_local", "o_entry_d"],
+                      "o_ol_cnt", "o_all_local", "o_entry_d"],
             # Item
             'item': ["i_id", "i_name", "i_price", "i_im_id", "i_data"],
             # Orderline
             'order-line': ["w_id", "d_id", "o_id", "ol_number", "ol_i_id",
-                          "ol_delivery_d", "ol_amount", "ol_supply_w_id",
-                          "ol_quantity", "ol_dist_info"],
+                           "ol_delivery_d", "ol_amount", "ol_supply_w_id",
+                           "ol_quantity", "ol_dist_info"],
             # Stock
             'stock': ["w_id", "i_id", "s_quantity", "s_ytd", "s_order_cnt",
                       "s_remote_cnt", "s_dist_01", "s_dist_02", "s_dist_03",
@@ -53,8 +54,65 @@ class Data:
         dataframe.to_csv(filepath, na_rep="null", header=False,
                          index=False)
 
-    # TODO: Files to be Written
+    def get_full_filepath(self, filename):
+        return os.path.join(os.path.sep, conf['data-path'], filename)
+
+    def create_warehouse(self):
+        filepath = self.get_full_filepath("mongo_warehouse.csv")
+        df_warehouse = self.read_original_csv("warehouse")
+        processed_warehouse = df_warehouse[["w_id", "w_name", "w_street_1",
+                                            "w_street_2", "w_city", "w_state",
+                                            "w_zip", "w_ytd"]]
+        self.debug("Processed {}: {}\n".format(filepath,
+                                               processed_warehouse.shape))
+        self.helper_write_csv(processed_warehouse, filepath)
+
+    def create_district(self):
+        filepath = self.get_full_filepath("mongo_district.csv")
+        df_warehouse = self.read_original_csv("warehouse")
+        df_district = self.read_original_csv("district")
+        processed_district = pd.merge(df_district, df_warehouse,
+                                      on="w_id", how='left')
+        processed_district = processed_district[["w_id", "d_id", "d_name",
+                                                 "d_street_1", "d_street_2",
+                                                 "d_city", "d_state", "d_zip",
+                                                 "w_tax", "d_tax", "d_ytd",
+                                                 "d_next_o_id"]]
+        self.debug("Processed {}: {}\n".format(filepath,
+                                               processed_district.shape))
+        self.helper_write_csv(processed_district, filepath)
+
+    def create_order(self):
+        pass
+
+    def create_customer(self):
+        filepath = self.get_full_filepath("mongo_customer.csv")
+        df_customer = self.read_original_csv("customer")
+        self.debug("Processed {}: {}\n".format(filepath,
+                                               df_customer.shape))
+        self.helper_write_csv(df_customer, filepath)
+
+    def create_stock(self):
+        filepath = self.get_full_filepath("mongo_stock.csv")
+        df_stock = self.read_original_csv("stock")
+        df_item = self.read_original_csv("item")
+        processed_stock = pd.merge(df_stock, df_item, on="i_id", how='left')
+        processed_stock = processed_stock[["w_id", "i_id", "s_quantity",
+                                           "s_ytd", "s_order_cnt",
+                                           "s_remote_cnt", "s_dist_01",
+                                           "s_dist_02", "s_dist_03",
+                                           "s_dist_04", "s_dist_05",
+                                           "s_dist_06", "s_dist_07",
+                                           "s_dist_08", "s_dist_09",
+                                           "s_dist_10", "s_data", "i_name",
+                                           "i_price", "i_im_id", "i_data"]]
+        self.debug("Processed {}: {}\n".format(filepath,
+                                               processed_stock.shape))
+        self.helper_write_csv(processed_stock, filepath)
 
     def preprocess(self):
-        # TODO: Files to be Written
-        pass
+        self.create_warehouse()
+        self.create_district()
+        self.create_order()
+        self.create_customer()
+        self.create_stock()
